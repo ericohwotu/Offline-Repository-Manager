16c920f08c1dce8583dc44620074c686bb275138
Vagrant <vagrant@EricHost>
2017-07-07 04:58:38 -0500

    opm-create added

--- opm.sh
+++ opm.sh
@@ -3,26 +3,26 @@
 
 if [ $1 == "--create" ]
   then
-    opm-create $2 $3
+    ./opm-create $2 $3
 elif [ $1 == "--add" ]
   then
-    opm-add $2 $3
+    ./opm-add $2 $3
 elif [ $1 == "--root" ] 
   then
-    opm-root $2 $3
+    ./opm-root $2 $3
 elif [ $1 == "--source" ]
   then
-    opm-src $2 $3
+    ./opm-src $2 $3
 elif [ $1 == "--save" ]
   then
-    opm-save $2
+    ./opm-save $2
 elif [ $1 == "--commit" ]
   then
-    opm-commit $2
+    ./opm-commit $2
 elif [ $1 == "--info" ] 
   then
-    opm-info $2
+    ./opm-info $2
 elif [ $1 == "--help" ] 
   then
-    opm-hlp
+    ./opm-hlp
 fi
