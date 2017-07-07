b28bbb8e17fae2d1bc2d57adb210294f84516585
Vagrant <vagrant@EricHost>
2017-07-07 04:44:29 -0500

    opm initial

---
+++ opm.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Refer to help for usage
+
+if [ $1 == "--create" ]
+  then
+    opm-create $2 $3
+elif [ $1 == "--add" ]
+  then
+    opm-add $2 $3
+elif [ $1 == "--root" ] 
+  then
+    opm-root $2 $3
+elif [ $1 == "--source" ]
+  then
+    opm-src $2 $3
+elif [ $1 == "--save" ]
+  then
+    opm-save $2
+elif [ $1 == "--commit" ]
+  then
+    opm-commit $2
+elif [ $1 == "--info" ] 
+  then
+    opm-info $2
+elif [ $1 == "--help" ] 
+  then
+    opm-hlp
+fi
